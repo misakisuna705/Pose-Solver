c332604e9b7311dfc0df6baa363f061b741b1ec4
misakisuna705 <misakisuna705@gmail.com>
2021-12-25 22:01:23 +0800

    commit

--- src/App/workspace/workspace.js
+++ src/App/workspace/workspace.js
@@ -13,19 +13,8 @@ import TimeSlice from "App/workspace/ctrl/timeSlice.js";
 import Playback from "App/workspace/ctrl/playback.js";
 //import ButtonToggler from "App/workspace/ctrl/buttonToggler.js";
 
-//import COACH_SKELETON from "assets/bvh/data_3d1.bvh";
-//import PLAYER_SKELETON from "assets/bvh/data_3d2.bvh";
 import COACH_SKELETON from "assets/coach/skeleton.bvh";
-import PLAYER_SKELETON from "assets/player/0821/2/pretest/user_second_test1_Char00.bvh";
-
-//import PLAYER_SKELETON from "assets/player/skeleton1.bvh";
-//import PLAYER_SKELETON from "assets/coach/skeleton.bvh";
-//import PLAYER_SKELETON from "assets/player/skeleton2.bvh";
-//import PLAYER_SKELETON from "assets/player/skeleton3.bvh";
-//import COACH_SKIN from "assets/coach3/skin.fbx";
-//import PLAYER_SKIN from "assets/coach2/skin.fbx";
-//import COACH_RACKET from "assets/coach3/racket.fbx";
-//import PLAYER_RACKET from "assets/coach2/racket.fbx";
+import PLAYER_SKELETON from "assets/player/skeleton.bvh";
 
 //import XNECTURL from "assets/xnect/post_raw3D.txt";
 
